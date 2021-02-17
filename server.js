1c3552af9f658689d057b682be492f99441adb7c
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-02-17 00:59:12 -0800

    handle post requests

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+//fs is required to write new animal data to animals.json:
+const fs = require('fs')
+//path - built in node module for working with file and directly paths.
+const path = require('path')
+
 const express = require('express')
 
 const PORT = process.env.PORT || 3001
 // 1. instantiate the server
 const app = express()
+//10. tell express.js app to intercept POST request before it gets to callback function. data will be run through function to take raw data transfered over HTTP and convert it to JSON object:
+//parse incoming string or array data:
+app.use(express.urlencoded({ extended: true }))
+//parse incoming JSOn data:
+app.use(express.json())
+
+//express.urlencoded = method that takes incoming POt data and converts to key value pairs accessible by `req.body` object
+//extended: true = tells server to look for sub-array data in the POST to amke sure it all gets parsed correctly.
+//`express.json()` method takes incoming POSt data in JSON form and parses it into `req.body` JavasScript object. Both express.urlencoded() and express.json() are middleware functions that need to be set up every time you create a server taht's looking to accept POST data.
+
 // 3. create route that front-end can request data from
 const { animals } = require('./data/animals')
 // 3-1. To add to the route:
@@ -74,6 +89,21 @@ app.get('/api/animals', (req, res) => {
   }
   res.json(results)
 })
+
+//set up route on server that accepts data to be used or stored server-side:
+app.post('/api/animals', (req, res) => {
+  //12. set id based on what the next index of the array will be
+  req.body.id = animals.length.toString()
+  //16. if any data in req.body is incorrect, send 400 error back:
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.')
+  } else {
+    //13. add animal to json file and animals array in this function:
+    const animal = createNewAnimal(req.body, animals)
+    res.json(animal)
+  }
+})
+
 // 7. unlike the query object, the param object needs to be defined in the route path, with route/:<parameterName>. create a new GET route for animals, this time adding :id to the end of the route. This will give us multiple routes.
 app.get('/api/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals)
@@ -84,6 +114,41 @@ app.get('/api/animals/:id', (req, res) => {
     res.send(404)
   }
 })
+
+//11. create separate function to handle taking data from `req.body` and adding it to animals.json file (this adds new animals to the catalog!):
+function createNewAnimal(body, animalsArray) {
+  //13. when we POST a new animal, add it to the imported `animals` array from the `animals.json` file.
+  const animal = body
+  animalsArray.push(animal)
+  console.log(body)
+
+  //14. use fs writeFileSync method, which is synchronous version of fs.writeFile() and doesn't require a callback function. If writing to lager data set, async would work better. This writes our animals.json file in the data subdirectory so we use the mehod path.join() to join the value of the directory name of the file we execute the code in (current directory, most likely) with the path to the animals.json file.
+  //save JS array data as JSON using JSON.stringify() to convert it. `null` and `2` keep data formatted: null means if we don't want to edit existing data. 2 means we want to create whitespace between our values to make it more readable. Otherwise, the json file would be had to read.
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  )
+
+  //return finished code to post route for response
+  return animal
+}
+//createNewAnimal() acepts teh POST route's req-body value and the array we want to add data to--`animalsArray`.
+
+//15. add validation that takes new animal data from `req.body` and check if each key not only exists but that it's the right type of data:
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return true
+  }
+}
 
 //2. tell server to listen for requests:
 app.listen(PORT, () => {
